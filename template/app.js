0e5ee854ab8a084da75b8b3635f8d8717a706478
pyj <yepark98@gmail.com>
2023-03-02 18:21:04 +0900

    nav class 추가 및 삭제

--- template/app.js
+++ template/app.js
@@ -1,1 +1,12 @@
 // do something!
+const navElement = document.getElementById("activeNav");
+const btnElement = document.getElementById("toggleButton");
+
+const btnClick = () => {
+  const className = navElement.classList;
+  if (className.contains("active")) {
+    navElement.classList.toggle("active", false);
+  } else {
+    navElement.classList.toggle("active", true);
+  }
+};
